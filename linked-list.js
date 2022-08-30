9b5ea56bba166af7a6ff3d8bb80504f416ae2147
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-08-30 10:18:27 -0400

    Implement singly linked list

--- linked-list.js
+++ linked-list.js
@@ -7,15 +7,28 @@ class LinkedListNode {
 
 class LinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    const node = new LinkedListNode(val);
+    node.next = this.head;
+    this.head = node;
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    this.length++;
+    if (!this.head) {
+      this.head = new LinkedListNode(val);
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = new LinkedListNode(val);
   }
 
   // You can use this function to help debug
