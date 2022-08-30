6db5afb7fa795c4c3670b2286798158b2586660c
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-08-30 10:45:50 -0400

    Implement timing tests for linked list functions

--- linked-list-timing.js
+++ linked-list-timing.js
@@ -6,4 +6,55 @@ Construct a timing test to verify the time complexities of `addToHead` and
 `addToTail` for both singly and doubly linked lists.
 */
 
-// Your code here
+// Singly Linked List: addToHead()
+let linkedList = new LinkedList();
+for (let i = 0; i < 10; i++) {
+  const startTime = Date.now();
+  for (let i = 0; i < 1000; i++) {
+    linkedList.addToHead(i);
+  }
+  console.log(Date.now() - startTime);
+}
+// Time Complexity: O(1)
+
+console.log("--------");
+
+// Singly Linked List: addToTail()
+linkedList = new LinkedList();
+for (let i = 0; i < 10; i++) {
+  const startTime = Date.now();
+  for (let i = 0; i < 1000; i++) {
+    linkedList.addToTail(i);
+  }
+  console.log(Date.now() - startTime);
+}
+
+// Time Complexity: O(n)
+
+console.log("--------");
+
+// Doubly Linked List: addToHead()
+let doublyLinkedList = new DoublyLinkedList();
+for (let i = 0; i < 10; i++) {
+  const startTime = Date.now();
+  for (let i = 0; i < 1000; i++) {
+    doublyLinkedList.addToHead(i);
+  }
+  console.log(Date.now() - startTime);
+}
+
+// Time Complextiy: O(1)
+
+console.log("--------");
+
+// Doubly Linked List: addToTail()
+doublyLinkedList = new DoublyLinkedList();
+for (let i = 0; i < 10; i++) {
+  const startTime = Date.now();
+  for (let i = 0; i < 1000; i++) {
+    doublyLinkedList.addToTail(i);
+  }
+  console.log(Date.now() - startTime);
+}
+
+// Time Complextiy: O(1)
