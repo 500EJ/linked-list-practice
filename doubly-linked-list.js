bafe8ef5e4431f618f4b4a23690400b07c1c0976
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-08-30 10:26:02 -0400

    Implement doubly linked list

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -8,15 +8,30 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    const node = new DoublyLinkedListNode(val);
+    node.next = this.head;
+    if (this.head) this.head.prev = node;
+    this.head = node;
+    if (!this.tail) this.tail = this.head;
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    this.length++;
+    if (!this.tail) {
+      this.tail = new DoublyLinkedListNode(val);
+      this.head = this.tail;
+      return;
+    }
+    this.tail.next = new DoublyLinkedListNode(val);
+    this.tail.next.prev = this.tail;
+    this.tail = this.tail.next;
   }
 
   // You can use this function to help debug
